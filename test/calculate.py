91a99fe7a57896d55361209423da70cfbacacf12
miyuki-shirogane <miyuk1@126.com>
2022-05-10 09:11:07 +0800

    update test

--- test/calculate.py
+++ test/calculate.py
@@ -9,18 +9,18 @@ class Calculate:
         data = [x for x in [a, b, c] if type(x) == int]
         if len(data) == 3:
             if a+b+c != 5:
-                print("err")
+                return "err"
             elif a+b+c == 5:
                 for i in [a, b, c]:
                     if i > 0:
                         res.append(i)
                     else:
                         pass
-                print(res)
+                return res
         elif len(data) != 3:
-            print("数据类型错误")
+            return "数据类型错误"
 
 
 if __name__ == '__main__':
     cc = Calculate()
-    cc.cal(-1, 1, 5)
+    print(cc.cal(-1, 1, 5))
